eae22f36c4bc4d6d69bfa3fb2927ae80bccc5e65
Xin Zheng <jason.zhengxin@gmail.com>
2020-05-31 10:50:36 +1000

    update project

--- stats/build/index.js
+++ stats/build/index.js
@@ -4,8 +4,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = __importDefault(require("fs"));
-var matches = fs_1.default.readFileSync('./data/football.csv', {
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+// use enum
+var matches = fs_1.default
+    .readFileSync('./data/football.csv', {
     encoding: 'utf-8'
-}).split('\n').map(function (row) { return row.split(','); });
-console.log('matches', matches);
+})
+    .split('\n')
+    .map(function (row) { return row.split(','); });
+var manUnitedWinds = 0;
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWinds++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWinds++;
+    }
+}
+console.log("Man United won " + manUnitedWinds + " games");
 //# sourceMappingURL=index.js.map
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,7 +1,28 @@
 import fs from 'fs';
 
-const matches = fs.readFileSync('./data/football.csv', {
-  encoding: 'utf-8'
-}).split('\n').map((row:string):string[] => row.split(','));
+enum MatchResult {
+	HomeWin = 'H',
+	AwayWin = 'A',
+	Draw = 'D'
+}
 
-console.log('matches',matches);
+// use enum
+
+const matches = fs
+	.readFileSync('./data/football.csv', {
+		encoding: 'utf-8'
+	})
+	.split('\n')
+	.map((row: string): string[] => row.split(','));
+
+let manUnitedWinds = 0;
+
+for (let match of matches) {
+	if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+		manUnitedWinds++;
+	} else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+		manUnitedWinds++;
+	}
+}
+
+console.log(`Man United won ${manUnitedWinds} games`);
