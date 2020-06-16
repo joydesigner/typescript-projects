256e644ac410ea1c982ea641d0ff68988f18d3b9
Xin Zheng <xin.zheng@anz.com>
2020-06-16 17:09:33 +1000

    use class composite to create app

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,20 +1,12 @@
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
 import { CsvFileReader } from './CsvFileReader';
-
-// const reader = new MatchReader('./data/football.csv')
-// reader.read();
+import {ConsoleReport} from './reportTargets/ConsoleReport';
+import {WinsAnalysis} from './analyzers/WinsAnalysis';
+import {Summary} from './Summary';
 
 const csvFileReader = new CsvFileReader('./data/football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-let manUnitedWinds = 0;
 
-for (let match of matchReader.matches) {
-	if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-		manUnitedWinds++;
-	} else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-		manUnitedWinds++;
-	}
-}
-console.log(`Man United won ${manUnitedWinds} games`);
+const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
