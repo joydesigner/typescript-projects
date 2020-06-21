861f48a4d4407c520a5a3776d50b4d13d7433b8a
Xin Zheng <jason.zhengxin@gmail.com>
2020-06-21 11:24:06 +1000

    update project

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,13 +1,8 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
-import { HtmlReport } from './reportTargets/HtmlReport';
 
-const csvFileReader = new CsvFileReader('./data/football.csv');
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('./data/football.csv');
+const summary = Summary.winAnalysisWithHtmlReport('Man United');
+
 matchReader.load();
-
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
